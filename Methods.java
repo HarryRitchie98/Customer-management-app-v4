54d9caaeaf3683cd98cf07123006937f8364c540
HarryRitchie98 <harry.ritchie@atos.net>
2022-04-25 21:39:34 +0100

    Create user feature

--- Methods.java
+++ Methods.java
@@ -21,18 +21,61 @@ import java.util.Scanner;
         public String passwordDB = "password";
 
 
-    public void MainMenuSelect() {
+        public void MainMenuSelect() {
 
-        //Main menu options
-        System.out.println("Select from the options below:");
-        System.out.println("Press 1 to add a customer to the system \n Press 2 to view all customers in the system \n " +
-                "Press 3 to update a customer in the system \n Press 4 to delete a customer in the system");
-        Scanner menuSelect = new Scanner(System.in);
-        String dataInput = menuSelect.nextLine();
+            //Main menu options
+            System.out.println("Select from the options below:");
+            System.out.println("Press 1 to add a customer to the system \n Press 2 to view all customers in the system \n " +
+                    "Press 3 to update a customer in the system \n Press 4 to delete a customer in the system");
+            Scanner menuSelect = new Scanner(System.in);
+            String dataInput = menuSelect.nextLine();
+
+            switch (dataInput) {
+                //Create User
+                case "1":
+                    try {
+                        //Connection to DB
+                        Class.forName("com.mysql.cj.jdbc.Driver");
+                        Connection connection = DriverManager.getConnection(jdbcURL, usernameDB, passwordDB);
+
+                        //Add customer options
+                        Scanner user1 = new Scanner(System.in);
+                        System.out.println("What is your username?");
+                        String username = user1.nextLine();
+                        System.out.println("What is your password?");
+                        String password = user1.nextLine();
+                        System.out.println("What is your email?");
+                        String email = user1.nextLine();
+                        System.out.println("What is your name?");
+                        String name = user1.nextLine();
+
+                        //Set SQL entries
+                        PreparedStatement statement = connection.prepareStatement(insertUsersSQL);
+                        statement.setString(1, username);
+                        statement.setString(2, password);
+                        statement.setString(3, email);
+                        statement.setString(4, name);
+
+                        //Update the SQL table
+                        int rows = statement.executeUpdate();
+
+                        //Confirmation of successful SQL entries
+                        if (rows > 0) {
+                            System.out.println("A new user has been added successfully");
+                        }
+                        connection.close();
+                        break;
+
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
 
 
+            }
+        }
     }
-}
 
 
 
