e015e055786e97f882c054b28b783e48bd0b6be6
HarryRitchie98 <harry.ritchie@atos.net>
2022-04-26 13:26:37 +0100

    Adding main method methods.java call

---
+++ UsersManager.java
@@ -0,0 +1,15 @@
+package main.java;
+
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class UsersManager {
+
+    public static void main(String[] args) throws SQLException {
+
+        Methods one = new Methods();
+        one.MainMenuSelect();
+    }
+}
+
+
